a502630d8c3c6a37b76ea1405202a3aeecc3482a
David Bustos <david@getmonetta.com>
2024-05-28 15:35:19 -0500

    se borran componentes sin usar

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import LooksOneIcon from "@mui/icons-material/LooksOne";
 import LooksTwoIcon from "@mui/icons-material/LooksTwo";
 import Looks3Icon from "@mui/icons-material/Looks3";
 import BARBERO from "../src/assets/BARBERO.webp";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 import { DatePickerComponent } from "./components/DatePickerComponent";
 import { Button, DatePickerProps, message } from "antd";
@@ -12,32 +11,7 @@ import { CustomerData } from "./components/CustomerData";
 import { useEffect, useState } from "react";
 import dayjs, { Dayjs } from "dayjs";
 import { client } from "./supabase/client";
-import {
-  Card,
-  CardActions,
-  CardContent,
-  CardMedia,
-  Collapse,
-  IconButton,
-  IconButtonProps,
-  Typography,
-  styled,
-} from "@mui/material";
-
-interface ExpandMoreProps extends IconButtonProps {
-  expand: boolean;
-}
-
-const ExpandMore = styled((props: ExpandMoreProps) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
-  marginLeft: "auto",
-  transition: theme.transitions.create("transform", {
-    duration: theme.transitions.duration.shortest,
-  }),
-}));
+import { Card, CardMedia, Typography } from "@mui/material";
 
 interface CardProps {
   id: string;
