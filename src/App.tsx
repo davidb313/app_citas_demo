a4ff7eb44bc6f59b4333eee645bea193901c5ea8
David Bustos <david@getmonetta.com>
2024-05-29 19:45:50 -0500

    ajustando logica para traer el nombre del barbero

--- src/App.tsx
+++ src/App.tsx
@@ -20,28 +20,110 @@ interface CardProps {
   setSelectedBarber: React.Dispatch<React.SetStateAction<string | null>>;
   selectedService: string | null;
   setSelectedService: React.Dispatch<React.SetStateAction<string | null>>;
+  setSelectedBarberName: React.Dispatch<React.SetStateAction<string | null>>;
 }
 
+const RecipeReviewCard: React.FC<CardProps> = ({
+  id,
+  name,
+  selectedBarber,
+  setSelectedBarber,
+  selectedService,
+  setSelectedService,
+  setSelectedBarberName,
+}) => {
+  const handleServiceClick = (service: string) => {
+    if (selectedBarber === null || selectedBarber === id) {
+      if (selectedService === service && selectedBarber === id) {
+        setSelectedService(null);
+        setSelectedBarber(null);
+        setSelectedBarberName(null);
+      } else {
+        setSelectedService(service);
+        setSelectedBarber(id);
+        setSelectedBarberName(name);
+      }
+    }
+  };
+
+  return (
+    <Card sx={{ maxWidth: "auto", marginTop: 3 }}>
+      <div className='flex'>
+        <CardMedia
+          component='img'
+          sx={{ height: "auto", width: 100, objectFit: "cover" }}
+          image={BARBERO}
+          alt='foto barbero'
+        />
+        <div className='ml-4'>
+          <Typography variant='h6' color='text.primary'>
+            {name}
+          </Typography>
+
+          <div className='mt-2 mb-2'>
+            <Typography variant='body2' color='text.secondary'>
+              Escoge un servicio:
+            </Typography>
+
+            <div className='flex flex-wrap mt-2 gap-2'>
+              <Button
+                type={
+                  selectedService === "corte" && selectedBarber === id
+                    ? "primary"
+                    : "default"
+                }
+                onClick={() => handleServiceClick("corte")}
+                disabled={selectedBarber !== null && selectedBarber !== id}
+                size='small' // Agrega esta línea para hacer el botón más pequeño
+              >
+                Corte de Cabello $20.000
+              </Button>
+              <Button
+                type={
+                  selectedService === "corte_barba" && selectedBarber === id
+                    ? "primary"
+                    : "default"
+                }
+                onClick={() => handleServiceClick("corte_barba")}
+                disabled={selectedBarber !== null && selectedBarber !== id}
+                size='small' // Agrega esta línea para hacer el botón más pequeño
+              >
+                Corte + Barba $23.000
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Card>
+  );
+};
+
 export default function App() {
-  //Barbers List
+  //Lista donde se guarda la info de todos los barberos
   const [allBarbers, setAllBarbers] = useState<any>([]);
 
+  //useState de la informacion de los barberos
   const [selectedBarber, setSelectedBarber] = useState<string | null>(null);
   const [selectedService, setSelectedService] = useState<string | null>(null);
+  const [selectedBarberName, setSelectedBarberName] = useState<string | null>(
+    null
+  );
 
-  //Date Picker
+  console.log(selectedBarberName);
+
+  //useState del Date y Time Picker
   const [selectedDate, setSelectedDate] = useState<Dayjs | null>(null);
   const [selectedButton, setSelectedButton] = useState<string | null>(null);
   const [selectedTime, setSelectedTime] = useState<Dayjs | null>(null);
 
-  //Customer Data
+  //useState de la info del cliente que toma el servicio
   const [customerName, setCustomerName] = useState("");
   const [customerNumber, setCustomerNumber] = useState("");
 
-  //Confirm Message
+  //useState del mensaje de confirmacion
   const [messageApi, contextHolder] = message.useMessage();
 
-  //Barbers Logic
+  //Traer a todos los barberos que se crean en Supabase
   const getAllBarbers = async () => {
     try {
       const response = await client.from("barberos").select();
@@ -53,86 +135,13 @@ export default function App() {
     }
   };
 
-  console.log(selectedTime);
-
+  //Este useEffect trae a todos los barberos apenas se carga la App
   useEffect(() => {
     getAllBarbers();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const RecipeReviewCard: React.FC<CardProps> = ({
-    id,
-    name,
-    selectedBarber,
-    setSelectedBarber,
-    selectedService,
-    setSelectedService,
-  }) => {
-    const handleServiceClick = (service: string) => {
-      if (selectedBarber === null || selectedBarber === id) {
-        if (selectedService === service && selectedBarber === id) {
-          setSelectedService(null);
-          setSelectedBarber(null);
-        } else {
-          setSelectedService(service);
-          setSelectedBarber(id);
-        }
-      }
-    };
-
-    return (
-      <Card sx={{ maxWidth: "auto", marginTop: 3 }}>
-        <div className='flex'>
-          <CardMedia
-            component='img'
-            sx={{ height: "auto", width: 100, objectFit: "cover" }}
-            image={BARBERO}
-            alt='foto barbero'
-          />
-          <div className='ml-4'>
-            <Typography variant='h6' color='text.primary'>
-              {name}
-            </Typography>
-
-            <div className='mt-2 mb-2'>
-              <Typography variant='body2' color='text.secondary'>
-                Escoge un servicio:
-              </Typography>
-
-              <div className='flex flex-wrap mt-2 gap-2'>
-                <Button
-                  type={
-                    selectedService === "corte" && selectedBarber === id
-                      ? "primary"
-                      : "default"
-                  }
-                  onClick={() => handleServiceClick("corte")}
-                  disabled={selectedBarber !== null && selectedBarber !== id}
-                  size='small' // Agrega esta línea para hacer el botón más pequeño
-                >
-                  Corte de Cabello $20.000
-                </Button>
-                <Button
-                  type={
-                    selectedService === "corte_barba" && selectedBarber === id
-                      ? "primary"
-                      : "default"
-                  }
-                  onClick={() => handleServiceClick("corte_barba")}
-                  disabled={selectedBarber !== null && selectedBarber !== id}
-                  size='small' // Agrega esta línea para hacer el botón más pequeño
-                >
-                  Corte + Barba $23.000
-                </Button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </Card>
-    );
-  };
-
-  //Date Picker logic
+  //Logica del Date y Time Picker
   const onChange: DatePickerProps["onChange"] = (date) => {
     setSelectedDate(date);
     setSelectedButton(null);
@@ -146,21 +155,17 @@ export default function App() {
     const today = dayjs();
     setSelectedDate(today);
     setSelectedButton("today");
-    setSelectedTime(null); // Reset selected time
+    setSelectedTime(null);
   };
 
   const setTomorrow = () => {
     const tomorrow = dayjs().add(1, "day");
     setSelectedDate(tomorrow);
     setSelectedButton("tomorrow");
-    setSelectedTime(null); // Reset selected time
+    setSelectedTime(null);
   };
 
-  /* const selectTime = (time: string) => {
-    setSelectedTime(time);
-  }; */
-
-  //Confirm Message
+  //Mensajes de confirmacion o error de la App
   const success = () => {
     messageApi.open({
       type: "success",
@@ -196,6 +201,7 @@ export default function App() {
         setCustomerNumber("");
         setSelectedBarber("");
         setSelectedService("");
+        setSelectedBarberName(null);
       }
     } catch (err) {
       error();
@@ -225,6 +231,7 @@ export default function App() {
               name={barber?.nombre_barbero}
               selectedBarber={selectedBarber}
               setSelectedBarber={setSelectedBarber}
+              setSelectedBarberName={setSelectedBarberName}
               selectedService={selectedService}
               setSelectedService={setSelectedService}
             />
@@ -233,7 +240,7 @@ export default function App() {
 
         <div className='mt-10 flex gap-x-2'>
           <LooksTwoIcon />
-          <h3 className='font-bold'>Selecciona el día</h3>
+          <h3 className='font-bold'>Selecciona el día y la hora</h3>
         </div>
         <DatePickerComponent
           selectedDate={selectedDate}
